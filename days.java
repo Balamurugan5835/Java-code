ca892798f969573171fbbb0d9bb600ca55567f00
Balamurugan5835 <balamurugan13june2006@gmail.com>
2025-11-09 13:29:20 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Days
+{
+
+
+
+    public static void main ( String[] args)
+    {
+    
+       Scanner ip=new Scanner(System.in);
+       
+        totdays=0;
+  
+}
+
+
+    
+
